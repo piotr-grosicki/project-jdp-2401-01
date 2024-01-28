4ffb43853d73e8bd2d16cbba4d14e42dba6eed0e
Patryk Krzyżak <p.krzyzak1986@gmail.com>
2024-01-28 12:28:59 +0100

    ProductController created

---
+++ src/main/java/com/kodilla/ecommercee/product/domain/Product.java
@@ -0,0 +1,16 @@
+package com.kodilla.ecommercee.product.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    private Long id;
+    private String name;
+    private String description;
+    private double price;
+}
