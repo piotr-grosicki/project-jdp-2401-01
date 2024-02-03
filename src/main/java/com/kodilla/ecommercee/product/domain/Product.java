d8f61631998877b5904b04fca6b8dde5cf505429
Kamil Wrobel <wrobelkam@gmail.com>
2024-02-03 10:25:46 +0100

    Encja Group - implementacja

---
+++ src/main/java/com/kodilla/ecommercee/product/domain/Product.java
@@ -0,0 +1,33 @@
+package com.kodilla.ecommercee.product.domain;
+
+import com.kodilla.ecommercee.productGroup.domain.ProductGroups;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Entity(name = "PRODUCTS")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NotNull
+    @Column(name = "ID", unique = true)
+    private Long productId;
+
+    @ManyToOne
+    @JoinColumn(name = "PRODUCTGROUP_ID")
+    private ProductGroups productGroups;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "PRICE")
+    private BigDecimal price;
+}
--- src/main/java/com/kodilla/ecommercee/productGroup/domain/ProductGroups.java
+++ src/main/java/com/kodilla/ecommercee/productGroup/domain/ProductGroups.java
@@ -1,15 +1,41 @@
 package com.kodilla.ecommercee.productGroup.domain;
 
+import com.kodilla.ecommercee.product.domain.Product;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "PRODCT_GROPS")
 public class ProductGroups {
 
-    private Long id;
+    @Id
+    @NotNull
+    @GeneratedValue
+    @Column(name = "PRODUCT_GROUP_ID", unique = true)
+    private Long productGroupId;
+
+    @NotNull
+    @Column(name = "PRODUCT_GROUP_NAME")
     private String name;
+
+    @Column(name = "PRODUCT_GROUP_DESCRIPTION")
     private String description;
+
+    @OneToMany(
+            targetEntity = Product.class,
+            mappedBy = "productGroups",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    private List<Product> productList = new ArrayList<>();
 }
