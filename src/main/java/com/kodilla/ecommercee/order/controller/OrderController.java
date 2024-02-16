9fa0a6a49116f21fad448cd6a827e7c7177f8e9e
Andrzej Maliszewski <adnrzej@netmb.pl>
2024-02-16 12:29:18 +0100

    JDP240101-24 OrderController

--- src/main/java/com/kodilla/ecommercee/order/controller/OrderController.java
+++ src/main/java/com/kodilla/ecommercee/order/controller/OrderController.java
@@ -26,11 +26,11 @@ public class OrderController {
         return ResponseEntity.ok(orderMapper.mapToOrderDtoList(orderList));
     }
     @GetMapping(value = "{orderId}")
-    public ResponseEntity<OrderDto> getOrder(@PathVariable long orderId) throws OrderNotFoundException{
+    public ResponseEntity<OrderDto> getOrder(@PathVariable Long orderId) throws OrderNotFoundException{
         return ResponseEntity.ok(orderMapper.mapToOrderDto(orderService.getOrder(orderId)));
     }
     @DeleteMapping(value = "{orderId}")
-    public ResponseEntity<Void> deleteOrder(@PathVariable long orderId){
+    public ResponseEntity<Void> deleteOrder(@PathVariable Long orderId){
         orderService.deleteOrder(orderId);
         return ResponseEntity.noContent().build();
     }
